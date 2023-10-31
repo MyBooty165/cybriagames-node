e997b15ac9e94c8e8f2523c6b53659be0eadb54d
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-10-31 15:59:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,35 @@
 const simpleGit = require('simple-git');
 const express = require('express');
 const { exec } = require('child_process');
-const fs = require('fs');
 const git = simpleGit();
 
 // Replace this with your GitHub repository URL
 const repoURL = 'https://github.com/CybriaTech/CybriaGames.git';
-const repoFolder = 'CybriaGames'; // The folder where the repository will be cloned
+const repoFolder = 'static'; // The folder where the repository will be cloned
 
 // Function to clone and update the repository
 const cloneAndPull = async () => {
-  // Clone the repository
-  await git.clone(repoURL);
+  // Clone the repository or update it if it already exists
+  const exists = fs.existsSync(repoFolder);
+  if (exists) {
+    await git.cwd(repoFolder).pull();
+    console.log('Repository already exists and was updated successfully.');
+  } else {
+    await git.clone(repoURL, repoFolder);
+    console.log('Repository cloned successfully.');
+  }
 
   // Periodically pull (update) the repository
   setInterval(async () => {
     try {
-      await git.pull();
+      await git.cwd(repoFolder).pull();
       console.log('Repository updated successfully.');
 
-      // Trigger a server restart when the repository is updated
-      restartServer();
+      // No need to restart the server; it will serve the updated contents automatically
     } catch (error) {
       console.error('Error updating repository:', error);
     }
   }, 60000); // Update every 1 minute (adjust the interval as needed)
-};
-
-// Function to restart the server
-const restartServer = () => {
-  console.log('Restarting server...');
-
-  // Stop the current server process
-  exec('pkill -f "node server.js"', (error, stdout, stderr) => {
-    if (error) {
-      console.error('Error stopping the server:', error);
-      return;
-    }
-
-    // Start a new server process
-    startServer();
-  });
 };
 
 // Function to start the server
@@ -57,3 +46,4 @@ const startServer = () => {
 
 // Call the cloneAndPull function to start the process
 cloneAndPull();
+startServer();
